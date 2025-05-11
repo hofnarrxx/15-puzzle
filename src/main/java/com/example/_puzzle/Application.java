af6b853dbcc947919a9f4d114db822c2478951f3
hofnarrxx <maksymparasiun@gmail.com>
2025-05-11 21:09:28 +0200

    proper env handling

--- src/main/java/com/example/_puzzle/Application.java
+++ src/main/java/com/example/_puzzle/Application.java
@@ -1,17 +1,27 @@
 package com.example._puzzle;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-    	System.setProperty("DB_USER", dotenv.get("DB_USER"));
-    	System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
-    	System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		File envFile = new File(".env");
+        if (envFile.exists()) {
+            Dotenv dotenv = Dotenv.load();
+            System.setProperty("DB_USER", dotenv.get("DB_USER"));
+            System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+            System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        } else {
+            System.setProperty("DB_USER", System.getenv("DB_USER"));
+            System.setProperty("DB_PASS", System.getenv("DB_PASS"));
+            System.setProperty("DB_URL", System.getenv("DB_URL"));
+        }
 		SpringApplication.run(Application.class, args);
 	}
 
